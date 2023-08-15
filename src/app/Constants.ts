c692fe629ff6dddcdfe33aa024645694b6fe8efa
Aleks Zavyalov <zavyalov.aleks@gmail.com>
2023-08-15 11:39:26 +0300

    switch be url

--- src/app/Constants.ts
+++ src/app/Constants.ts
@@ -1,5 +1,6 @@
 export class Constants {
-  private static readonly BASE_URL = 'https://zany-ruby-zebra-cape.cyclic.app/';
+  private static readonly BASE_URL = 'https://fc-cvw.xyz/cvw-be';
+  // private static readonly BASE_URL = 'http://localhost:3000';
 
   public static readonly INSTRUMENTS_API = `${Constants.BASE_URL}/instrument`;
   public static readonly INSTRUMENT_API = (id: string) => `${Constants.INSTRUMENTS_API}/${id}`;
